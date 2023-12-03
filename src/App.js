ec6fe8ae3ee95fb6954190c60d0a463178ddafda
BhanuPratap0 <119663548+BhanuPratap0@users.noreply.github.com>
2023-12-03 22:12:35 +0530

    dark mode fix

--- src/App.js
+++ src/App.js
@@ -42,12 +42,12 @@ const App =(props)=> {
       <NavBar mode={mode} toggleMode={toggleMode} />
       <Routes>
       <Route exact path="/" element={<News mode={mode}  apiKey={apiKey} handleProgress={handleProgress} key="general" headline="World" pageSize={6} category="world"/>} ></Route>
-        <Route exact path="/arts" element={<News apiKey={apiKey}  handleProgress={handleProgress} key="arts" headline="Arts" pageSize={6} category="arts"/>} ></Route>
+        <Route exact path="/arts" element={<News mode={mode} apiKey={apiKey}  handleProgress={handleProgress} key="arts" headline="Arts" pageSize={6} category="arts"/>} ></Route>
 
-        <Route exact path="/home" element={<News apiKey={apiKey}  handleProgress={handleProgress} key="home" headline="Home" pageSize={6} category="home"/>} ></Route>
+        <Route exact path="/home" element={<News mode={mode} apiKey={apiKey}  handleProgress={handleProgress} key="home" headline="Home" pageSize={6} category="home"/>} ></Route>
 
-        <Route exact path="/us" element={<News apiKey={apiKey}  handleProgress={handleProgress} key="us" headline="US" pageSize={6} category="us"/>} ></Route>
-        <Route exact path="/technology" element={<News apiKey={apiKey}  handleProgress={handleProgress} key="technology" headline="Technology" pageSize={6} category="technology"/>} ></Route>
+        <Route exact path="/us" element={<News mode={mode} apiKey={apiKey}  handleProgress={handleProgress} key="us" headline="US" pageSize={6} category="us"/>} ></Route>
+        <Route exact path="/technology" element={<News mode={mode} apiKey={apiKey}  handleProgress={handleProgress} key="technology" headline="Technology" pageSize={6} category="technology"/>} ></Route>
       {/* 
       <Route exact path="/science" element={<News apiKey={apiKey}  handleProgress={handleProgress} key="science" headline="Science" pageSize={6} category="science"/>} ></Route>
         <Route exact path="/sports" element={<News apiKey={apiKey}  handleProgress={handleProgress} key="sports" headline="Sports" pageSize={6} category="sports"/>} ></Route>
